4baeef709b5bb346ca577e11f4ece368ff0a128c
Dorra Jaouad <dorra.jaouad@nextcloud.com>
2024-11-11 21:47:23 +0100

    test
    Signed-off-by: Dorra Jaouad <dorra.jaouad@nextcloud.com>

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './src/index.js', // Your entry point should import CSS or files that import it
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // Cleans up previous files in dist folder
+    clean: true,
+    publicPath: '/scholarship-roulette/',
   },
   plugins: [
     new Dotenv(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      inject: true, // Automatically inject scripts and styles
+      inject: true,
     }),
   ],
   module: {
